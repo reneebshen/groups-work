7c4b611c7122df0a5ae01227989aad9da0783aa6
reneebshen <rb18shen@gmail.com>
2020-07-16 14:56:43 -0400

    Added custom 404 and 500 error pages.

--- group_work/settings.py
+++ group_work/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'group_work.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
